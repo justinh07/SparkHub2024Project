f5ba7d7f90493189f2b1bf174fdd9030b5896e2b
justinh07 <justin.x.huang.sf@gmail.com>
2024-06-18 14:57:02 -0700

    Initial Commit

---
+++ SparkHub2024/SparkHub2024App.swift
@@ -0,0 +1,17 @@
+//
+//  SparkHub2024App.swift
+//  SparkHub2024
+//
+//  Created by Justin Huang on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SparkHub2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SparkHub2024UITests/SparkHub2024UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SparkHub2024UITestsLaunchTests.swift
+//  SparkHub2024UITests
+//
+//  Created by Justin Huang on 6/18/24.
+//
+
+import XCTest
+
+final class SparkHub2024UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
