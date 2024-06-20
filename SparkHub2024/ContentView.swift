fccba9f7c5c118f1fa916189092c58cfaeae8bfd
chris hsu <christopherwhsu@gmail.com>
2024-06-19 18:12:54 -0700

    Changed location to a variable, eventually gonna make it use user location

--- SparkHub2024/ContentView.swift
+++ SparkHub2024/ContentView.swift
@@ -10,6 +10,9 @@ import MapKit
 
 struct ContentView: View {
     @State private var bgcolor: Color = .blue
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+
+    
     var body: some View {
         ZStack {
             bgcolor.edgesIgnoringSafeArea(.all)
@@ -20,8 +23,9 @@ struct ContentView: View {
             
         }
         .offset(CGSize(width: 0, height: -250))
-        Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))), interactionModes: [])
-            .frame(width: 400, height: 300)
+        
+        Map(coordinateRegion: $region)
+            .frame(width: 400,height: 300)
             
     }
 }
