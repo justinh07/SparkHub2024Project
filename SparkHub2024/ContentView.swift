e4bf21285b31a9092bec841c5b9136edd8bf7002
justinh07 <justin.x.huang.sf@gmail.com>
2024-06-20 14:46:40 -0700

    contentview change

--- SparkHub2024/ContentView.swift
+++ SparkHub2024/ContentView.swift
@@ -8,17 +8,24 @@
 import SwiftUI
 import MapKit
 
-struct ContentView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+//struct ContentView: View {
+//    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+//
+//    
+//    var body: some View {
+//        ZStack {
+//            Map(coordinateRegion: $region)
+//                .frame(width: 500,height: 1000)
+//            }
+//        }
+//    }
 
-    
+
+struct ContentView: View {
     var body: some View {
-        ZStack {
-            Map(coordinateRegion: $region)
-                .frame(width: 500,height: 1000)
-            }
-        }
+        MapView()
     }
+}
 
 #Preview {
     ContentView()
