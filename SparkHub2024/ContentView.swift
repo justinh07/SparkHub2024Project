375a49df82d722975b8e0ab4b1e04a2fadd9b496
chris hsu <christopherwhsu@gmail.com>
2024-06-19 18:32:41 -0700

    - Made map full screen - Moved title to top with box

--- SparkHub2024/ContentView.swift
+++ SparkHub2024/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    
     @State private var bgcolor: Color = .blue
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
 
@@ -16,17 +17,25 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             bgcolor.edgesIgnoringSafeArea(.all)
-            VStack {
+            
+            Map(coordinateRegion: $region)
+                .frame(width: 500,height: 1000)
+            
+            ZStack {
+                Rectangle()
+                    .fill(Color.white)
+                    .frame(width: 500, height: 150)
+                    .opacity(0.8)
                 Text("SparkHub 2024 Hackathon")
                     .font(.system(size: 30))
+                    .offset(CGSize(width: 0, height: 50))
             }
-            
-        }
-        .offset(CGSize(width: 0, height: -250))
+            .offset(CGSize(width: 0, height: -450))
         
-        Map(coordinateRegion: $region)
-            .frame(width: 400,height: 300)
-            
+        
+        
+
+        }
     }
 }
 
