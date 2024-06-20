a6f6decfca1c5156694ba3d5f82977356e042f46
justinh07 <justin.x.huang.sf@gmail.com>
2024-06-20 14:40:07 -0700

    new view

---
+++ SparkHub2024/MapView.swift
@@ -0,0 +1,23 @@
+//
+//  MapView.swift
+//  SparkHub2024
+//
+//  Created by Justin Huang on 6/20/24.
+//
+
+import SwiftUI
+import MapKit
+
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}
