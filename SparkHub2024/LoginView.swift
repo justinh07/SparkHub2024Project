cdfe675553840c3d42708feb589269d5360fa0f1
justinh07 <justin.x.huang.sf@gmail.com>
2024-06-21 14:45:18 -0700

    profileview

---
+++ SparkHub2024/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  SparkHub2024
+//
+//  Created by Justin Huang on 6/21/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login")
+    }
+}
+
+
+#Preview {
+    LoginView()
+}
--- SparkHub2024/ProfileView.swift
+++ SparkHub2024/ProfileView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var karmaColor: Color = .green
+
     var body: some View {
-        Text("Profile")
+        NavigationStack {
+            VStack {
+                Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .padding()
+                    .foregroundColor(.black)
+                Text("Christopher Hsu")
+                    .foregroundColor(.black)
+                    .font(.system(size: 30))
+                    .padding()
+                VStack {
+                    Text("1000")
+                        .font(Font.system(size:60, design: .default))
+                        .foregroundColor(karmaColor)
+                    Text("Karma")
+                        .font(.system(size: 30))
+                        .foregroundColor(.black)
+                    }
+                .offset(y:50)
+                Menu {
+                    Button("Cancel") {}
+                    NavigationLink(destination: LoginView()) {
+                        Text("Confirm Log Out")
+                        .navigationBarBackButtonHidden(true)
+                    }
+                } label: {
+                    Text("Log Out")
+                        .font(.system(size: 20))
+                }
+                .offset(y: 280)
+            }
+            .offset(y: -100)
+        }
     }
 }
 
